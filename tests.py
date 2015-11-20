6922c870d8e4e36674b6a2f1fee567e0b63b936a
reenu renjith <reenu@rawdatatech.com>
2015-11-20 17:02:08 +0530

    first_submission

---
+++ tests.py
@@ -0,0 +1,14 @@
+# Create your tests here.
+import datetime
+
+from django.utils import timezone
+from django.test import TestCase
+
+from .models import WorkItem
+
+
+class WorkItemTests(TestCase):
+
+    def work_item_test_case_1(self):
+        pass
+        
